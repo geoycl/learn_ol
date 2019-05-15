160dbe295e868f71867163517251960d8d8da09f
geoycl <48855271+geoycl@users.noreply.github.com>
2019-05-15 17:47:26 +0800

    Update app.js

--- plot4ol3/js/app.js
+++ plot4ol3/js/app.js
@@ -89,7 +89,7 @@
     // 矢量图层
 	var vectorLayer = new ol.layer.Vector({
         source: new ol.source.Vector({
-            url: '../data/geojson/guizhou.geojson',
+            url: 'https://geoycl.github.io/learn_ol/plot4ol3/data/geojson/guizhou.geojson',
             format: new ol.format.GeoJSON(),
             wrapX: false
         }),
